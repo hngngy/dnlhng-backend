ebde77f10c09539e9739f52f0ee3f2ba93c7d929
Hoang Nguyen <hoang_ngy@yahoo.de>
2024-05-27 20:00:58 +0200

    chat fix

--- src/main/java/dnlhng/webtech/dnlhng/web/ChatController.java
+++ src/main/java/dnlhng/webtech/dnlhng/web/ChatController.java
@@ -1,32 +1,24 @@
 package dnlhng.webtech.dnlhng.web;
 
 import dnlhng.webtech.dnlhng.web.api.Nachricht;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/chat")
 public class ChatController {
 
     private final List<Nachricht> nachrichten = new ArrayList<>();
 
     @GetMapping
-    public String alleNachrichten(Model model) {
-        model.addAttribute("nachrichten", nachrichten);
-        return "chat";
+    public List<Nachricht> alleNachrichten() {
+        return nachrichten;
     }
 
     @PostMapping
-    public String sendeNachricht(@RequestParam("benutzername") String benutzername, @RequestParam("text") String text, Model model) {
-        nachrichten.add(new Nachricht(benutzername, text));
-        model.addAttribute("nachrichten", nachrichten);
-        return "redirect:/chat";
+    public void sendeNachricht(@RequestBody Nachricht nachricht) {
+        nachrichten.add(nachricht);
     }
 }
