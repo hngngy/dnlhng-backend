7f2ae580617bfcb86fd5f2d1928eb7fc76fd9ab2
Hoang Nguyen <hoang_ngy@yahoo.de>
2024-06-29 20:16:08 +0200

    test

--- src/main/java/dnlhng/webtech/dnlhng/web/api/Post.java
+++ src/main/java/dnlhng/webtech/dnlhng/web/api/Post.java
@@ -1,8 +1,14 @@
 package dnlhng.webtech.dnlhng.web.api;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
@@ -11,34 +17,12 @@ public class Post {
     private String username;
     private String message;
 
-    public Post(){}
 
-    public Post(String username, String message){
+    public Post(String username, String message) {
         this.username = username;
-        this.message = message;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
+
+
+
